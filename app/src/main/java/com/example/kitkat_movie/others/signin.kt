52c327b9da9552ad0ea9927d6d2914a2524b5fa9
Anna <anna.ecomclips@gmail.com>
2022-01-12 17:44:24 +0600

    fix login and signup update

--- app/src/main/java/com/example/kitkat_movie/others/signin.kt
+++ app/src/main/java/com/example/kitkat_movie/others/signin.kt
@@ -149,9 +149,4 @@ class signin : AppCompatActivity() {
             startActivity(intent)
         }
     }
-    override fun onBackPressed() {
-        val intent = Intent(applicationContext, NavBar::class.java)
-        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-        startActivity(intent)
-    }
 }
--- app/src/main/java/com/example/kitkat_movie/others/signup.kt
+++ app/src/main/java/com/example/kitkat_movie/others/signup.kt
@@ -197,11 +197,4 @@ class signup : AppCompatActivity() {
             }
         }
     }
-    override fun onBackPressed() {
-        val intent = Intent(applicationContext, NavBar::class.java)
-        intent.flags =
-            Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-
-        startActivity(intent)
-    }
 }
